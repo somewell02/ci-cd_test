be7dd82715754cfd25f97531da3b1d17847e49ce
Somewell <samvel121202@mail.ru>
2022-10-20 15:40:05 +0300

    Change counter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -28,21 +28,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-    margin: 40px 0 0;
+h1 {
+    color: red;
 }
-
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-
-a {
-    color: #42b983;
+button {
+    background: black;
+    color:white;
+    border: 0;
+    border-radius: 0;
 }
 </style>
